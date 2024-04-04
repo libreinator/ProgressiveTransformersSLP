81dc880112bdfc199fd60e9a253b8e1826db9920
Pranav Jerry <libreinator@disroot.org>
2024-04-04 21:41:02 +0530

    update make_lemma.py

--- make_lemma.py
+++ make_lemma.py
@@ -8,13 +8,12 @@ def main(filename):
     with open(
         "../data_aud_text/" + filename + ".text", mode="r", encoding="utf-8"
     ) as nonreg_trg_file:
-        sentences = []
+        lemma = []
         # For Source, Target and FilePath
         for nonreg_trg_line in nonreg_trg_file:
 
             nonreg_trg_line = nonreg_trg_line.strip()
             src_wrds = nonreg_trg_line.split(" ")
-            lemma = []
             for w in src_wrds:
                 if wordnet_lemmatizer.lemmatize(w) == "wa":
                     lemma.append(w)
@@ -22,18 +21,13 @@ def main(filename):
                     lemma.append(w)
                 else:
                     lemma.append(wordnet_lemmatizer.lemmatize(w))
-            sentences.append(lemma)
 
             # print(lemma)
             # input()
     print("writing")
-    with open("new_" + filename + ".text", "w") as train_text, open(
-        "" + filename + ".words.txt", "w"
-    ) as words_file:
-        for line in sentences:
-            train_text.write(" ".join(line) + "\n")
-            for word in line:
-                words_file.write(word + "\n")
+    with open(filename + ".words.txt", "w", encoding="utf8") as words_file:
+        for word in lemma:
+            words_file.write(word + "\n")
 
 
 if __name__ == "__main__":
